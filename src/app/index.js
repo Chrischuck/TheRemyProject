e3282e3c21c446ecb8bed5e38c55794513b8115b
Chris <chrischuck35@gmail.com>
2019-02-09 14:17:47 -0800

    Adds basic data model

--- src/app/index.js
+++ src/app/index.js
@@ -14,8 +14,41 @@ const Landing = Loadable({
 import Header from '../components/header'
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props)
+    const cart = window.localStorage.getItem('cart') 
+    this.state = {
+      cart: cart ? JSON.parse(cart) : null,
+      address: '',
+      isLoggedIn: !!window.localStorage.getItem('email')
+    }
+  }
+
+  setCart = (item) => {
+    if (item) {
+      window.localStorage.setItem('cart', JSON.stringify(item))
+    } else {
+      window.localStorage.setItem('cart', '')
+    }
+    this.setState({ cart: item })
+  }
+
+  onTextChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  login = () => {
+    this.setState({ isLoggedIn: true })
+  }
+
+  logout = () => {
+    window.localStorage.setItem('email', '')
+    this.setState({ isLoggedIn: false })
+    navigate('/login')
+  }
 
   render() {
+    const { cart, address, isLoggedIn } = this.state
     return (
       <Fragment>
         <Header />
