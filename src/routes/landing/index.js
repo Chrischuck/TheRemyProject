606016d2124fbeed009f3751114bdc93c76f3b85
lyryan <ly.ryan@yahoo.com>
2019-02-09 20:56:33 -0800

    Adds landing button

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -8,6 +8,7 @@ class Landing extends React.Component {
         <div className='landing-content'>
           <h1 style={{fontSize: '4em'}}>Friends don't let friends waste. Recycle. </h1>
           <p style={{fontSize: '20px'}}>The leading service to deliver and recycle your LaxCroix</p>
+          <a style={{margin: '0', width: '170px'}} className='round-button'>Get Started</a>
         </div>
         <div className='landing-svg'>
         <Landingsvg/>
