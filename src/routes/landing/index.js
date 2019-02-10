7a0d2090f287743f9e34e5186fa852965cac0e1b
lyryan <ly.ryan@yahoo.com>
2019-02-09 16:43:17 -0800

    adds landing work

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import Landingsvg from '../../svg/landing'
 
-export default () => (
-  <div>Landing</div>
-)
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className='landing-parent'>
+        <div className='landing-content'>
+          <h1>Headline</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div className='landing-svg'>
+        <Landingsvg/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Landing;
