aedb4bcc8d89a2067b3a6dda3dde2c5cc019c0e0
lyryan <ly.ryan@yahoo.com>
2019-02-10 09:19:13 -0800

    Adds mobile responsive landing page

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -4,10 +4,10 @@ import Landingsvg from '../../svg/landing'
 class Landing extends React.Component {
   render() {
     return (
-      <div className='landing-parent'>
+      <div style={{padding: '20px'}} className='landing-parent'>
         <div className='landing-content'>
-          <h1 style={{fontSize: '4em'}}>Friends don't let friends waste. Recycle. </h1>
-          <p style={{fontSize: '20px'}}>The leading service to deliver and recycle your LaxCroix</p>
+          <h1 style={{fontSize: '4em', margin: '5px 0'}}>Friends don't let friends waste. Recycle. </h1>
+          <p style={{fontSize: '25px'}}>The leading service to deliver and recycle your LaxCroix</p>
           <a style={{margin: '0', width: '170px'}} className='round-button'>Get Started</a>
         </div>
         <div className='landing-svg'>
